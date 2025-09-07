b6ff811c3e9194e97853fb5f35eb90e2da624582
Holger Benl <hbenl@evandor.de>
2025-09-07 18:38:40 +0200

    build docs with typedoc

--- src/core/objects/index.ts
+++ src/core/objects/index.ts
@@ -2,6 +2,7 @@ export { Status } from './status.js';
 export { Statistics } from './statistics.js';
 export { StoredPlaylist, PlaylistItem, SongIdAndPosition } from './playlists.js';
 export { File, Song, Playlist, Directory, DirectoryEntry, SongCount, GroupedSongCount } from './database.js';
+export type { DirectoryEntryType } from './database.js';
 export { OutputDevice } from './outputDevice.js';
 export { Mount, Neighbor } from './mount.js';
 export { Decoder } from './decoder.js';
